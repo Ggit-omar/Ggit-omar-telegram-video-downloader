34d2b23cb504629a7ca3160b642ee13d1b87b290
Ggit-omar <gghit450@gmail.com>
2025-06-18 16:46:35 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+BOT_TOKEN = os.getenv("7346139723:AAG1CR0qJ8mvlH6jXldVIzYDqSsj76F-2Q0")
 CHANNEL_ID = os.getenv("1002883125510")
 ADMIN_ID = int(os.getenv("ADMIN_ID", "7170990925"
 GROUP_ID = int(os.getenv("GROUP_ID", "1002491751710")
