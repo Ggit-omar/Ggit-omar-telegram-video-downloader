15bec2e03c8a9da0da2029ee66aa422ddefdd4b4
Ggit-omar <gghit450@gmail.com>
2025-06-18 16:30:07 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,6 @@
 import os
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-CHANNEL_ID = os.getenv("CHANNEL_ID")
-ADMIN_ID = int(os.getenv("ADMIN_ID", "0"))
-
-if not BOT_TOKEN or not CHANNEL_ID or ADMIN_ID == 0:
-    raise ValueError("Missing environment variables. Please set BOT_TOKEN, CHANNEL_ID, ADMIN_ID in Railway.")
+CHANNEL_ID = os.getenv("1002883125510")
+ADMIN_ID = int(os.getenv("ADMIN_ID", "7170990925"
+GROUP_ID = int(os.getenv("GROUP_ID", "1002491751710")
